67e2a86c29c5c5a7699c592af75d60bedd0160c8
chandan kumar maurya <code.us.cm@gmail.com>
2024-07-11 11:19:11 +0530

    Setup tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+// tailwind.config.js
+
+module.exports = {
+  ontent: [],
+  content: ["./App.{js,jsx,ts,tsx}", "./<custom directory>/**/*.{js,jsx,ts,tsx}"],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
